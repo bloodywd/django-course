e1bd039f7cc95d4196496401052c407fb4b26929
bloodywd <bloodyforreal@gmail.com>
2024-04-02 18:30:11 +0300

    create views, templates

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-($g++01_oi01l!zr73p^=e*=$uk(n9z)xcyucd+t0=fv2z+rvl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sitewomen',
     'sitewomen.women',
 ]
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'sitewomen.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,10 +1,32 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 def index(request):
-    return HttpResponse('Страница приложения women')
+    return render(request, 'women/index.html')
 
 
-def categories(request):
-    return HttpResponse('<h1>Статья по категориям</h1>')
+def about(request):
+    return render(request, 'women/about.html')
+
+
+def categories(request, cat_id):
+    return HttpResponse(f'<h1>Статья по категориям</h1><p>id : {cat_id}</p>')
+
+
+def categories_by_slug(request, cat_slug):
+    if request.GET:
+        print(request.GET)
+    return HttpResponse(f'<h1>Статья по категориям</h1><p>slug : {cat_slug}</p>')
+
+
+def archive(request, year):
+    if year > 2023:
+        url = reverse('cats', args=('sport', ))
+        return HttpResponsePermanentRedirect(url)
+    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
