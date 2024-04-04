230ff3e2418441d593bf3c7e3deac68aafcb5f45
bloodywd <bloodyforreal@gmail.com>
2024-04-05 02:28:55 +0300

    update

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -2,13 +2,16 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpRespons
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+
 
 def index(request):
-    return render(request, 'women/index.html')
+    data = {'title': 'Главная страница'}
+    return render(request, 'women/index.html', context=data)
 
 
 def about(request):
-    return render(request, 'women/about.html')
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
 def categories(request, cat_id):
@@ -16,14 +19,12 @@ def categories(request, cat_id):
 
 
 def categories_by_slug(request, cat_slug):
-    if request.GET:
-        print(request.GET)
     return HttpResponse(f'<h1>Статья по категориям</h1><p>slug : {cat_slug}</p>')
 
 
 def archive(request, year):
     if year > 2023:
-        url = reverse('cats', args=('sport', ))
+        url = reverse('cats', args=('sport',))
         return HttpResponsePermanentRedirect(url)
     return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
 
